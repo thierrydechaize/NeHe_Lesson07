4b6e4aa0561155e3dfa3e7d38aa7b9ad653c41f0
Thierry Dechaize <thierry.dechaize@sfr.fr>
2015-12-23 18:32:09 +0100

    Next Lesson07 from Tom Davis and Jef Molohee (NeHe). One paradigm : one source for many free compilers C/C++ on windows Signed-off-by: Thierry Dechaize <thierry.dechaize@sfr.fr>

---
+++ doxygen/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "_br",
+  1: "_b",
+  2: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files"
+};
+
+var indexSectionLabels =
+{
+  0: "Tout",
+  1: "Structures de données",
+  2: "Fichiers"
+};
+
